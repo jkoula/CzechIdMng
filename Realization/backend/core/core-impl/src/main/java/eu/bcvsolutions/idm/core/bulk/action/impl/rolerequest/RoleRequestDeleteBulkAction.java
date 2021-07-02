c797e6c6f2d590afd87aad29c39917c939e68978
Ondrej Husnik <ondrej.husnik@bcvsolutions.eu>
2021-07-02 10:07:00 +0200

    #2795 Remove bulk action for role-request agenda changes after review

--- Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/bulk/action/impl/rolerequest/RoleRequestDeleteBulkAction.java
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/bulk/action/impl/rolerequest/RoleRequestDeleteBulkAction.java
@@ -6,11 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Component;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 
 import eu.bcvsolutions.idm.core.api.bulk.action.AbstractRemoveBulkAction;
+import eu.bcvsolutions.idm.core.api.domain.CoreResultCode;
+import eu.bcvsolutions.idm.core.api.domain.OperationState;
+import eu.bcvsolutions.idm.core.api.domain.RoleRequestState;
+import eu.bcvsolutions.idm.core.api.dto.DefaultResultModel;
 import eu.bcvsolutions.idm.core.api.dto.IdmRoleRequestDto;
+import eu.bcvsolutions.idm.core.api.dto.OperationResultDto;
 import eu.bcvsolutions.idm.core.api.dto.filter.IdmRoleRequestFilter;
+import eu.bcvsolutions.idm.core.api.entity.OperationResult;
+import eu.bcvsolutions.idm.core.api.exception.RoleRequestException;
 import eu.bcvsolutions.idm.core.api.service.IdmRoleRequestService;
 import eu.bcvsolutions.idm.core.api.service.ReadWriteDtoService;
 import eu.bcvsolutions.idm.core.model.domain.CoreGroupPermission;
@@ -40,6 +48,39 @@ public class RoleRequestDeleteBulkAction extends AbstractRemoveBulkAction<IdmRol
 	@Override
 	protected List<String> getAuthoritiesForEntity() {
 		return Lists.newArrayList(CoreGroupPermission.ROLE_REQUEST_DELETE);
+	}
+	
+	@Override
+	protected OperationResult processDto(IdmRoleRequestDto dto) {
+		// Request in Executed state can not be delete or change
+		OperationResultDto systemState = dto.getSystemState();
+		if (RoleRequestState.EXECUTED == dto.getState() && systemState != null
+				&& OperationState.EXECUTED != systemState.getState()
+				&& OperationState.CANCELED != systemState.getState()) {
+			// Request was executed in IdM, but system state is not canceled -> we will change the system state to CANCELED.
+			OperationResultDto systemResult = new OperationResultDto.Builder(OperationState.CANCELED)
+					.setModel(new DefaultResultModel(CoreResultCode.ROLE_REQUEST_SYSTEM_STATE_CANCELED,
+							ImmutableMap.of("state", systemState != null ? systemState.getState().name() : "")))
+					.build();
+			dto.setSystemState(systemResult);
+			roleRequestService.save(dto);
+			return new OperationResult.Builder(OperationState.EXECUTED).build();
+		}
+		
+		// Request in Executed state can not be delete or change
+		if (RoleRequestState.EXECUTED == dto.getState()) {
+			throw new RoleRequestException(CoreResultCode.ROLE_REQUEST_EXECUTED_CANNOT_DELETE,
+					ImmutableMap.of("request", dto));
+		}
+		
+		// Only request in Concept state, can be deleted. In others states, will be
+		// Request set to Canceled state and save.
+		if (RoleRequestState.CONCEPT == dto.getState()) {
+			roleRequestService.delete(dto);
+		} else {
+			roleRequestService.cancel(dto);
+		}
+		return new OperationResult.Builder(OperationState.EXECUTED).build(); 
 	}
 	
 	@Override
--- Realization/backend/core/core-impl/src/test/java/eu/bcvsolutions/idm/core/bulk/action/impl/rolerequest/RoleRequestDeleteBulkActionIntegrationTest.java
+++ Realization/backend/core/core-impl/src/test/java/eu/bcvsolutions/idm/core/bulk/action/impl/rolerequest/RoleRequestDeleteBulkActionIntegrationTest.java
@@ -1,6 +1,5 @@
 package eu.bcvsolutions.idm.core.bulk.action.impl.rolerequest;
 
-
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -12,6 +11,8 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import eu.bcvsolutions.idm.core.api.bulk.action.dto.IdmBulkActionDto;
+import eu.bcvsolutions.idm.core.api.domain.OperationState;
+import eu.bcvsolutions.idm.core.api.domain.RoleRequestState;
 import eu.bcvsolutions.idm.core.api.dto.IdmIdentityContractDto;
 import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
 import eu.bcvsolutions.idm.core.api.dto.IdmRoleDto;
@@ -43,7 +44,7 @@ public class RoleRequestDeleteBulkActionIntegrationTest extends AbstractBulkActi
 	}
 	
 	@Test
-	public void processBulkActionByIds() {
+	public void deleteRoleRequestConcept() {
 		IdmRoleDto role = this.createRoles(1).get(0);
 		IdmIdentityDto identity = createIdentities(1).get(0);
 		IdmIdentityContractDto contract = getHelper().getPrimeContract(identity);
@@ -51,6 +52,7 @@ public class RoleRequestDeleteBulkActionIntegrationTest extends AbstractBulkActi
 		IdmRoleRequestDto roleRequest = roleRequestService.createRequest(contract, role);
 		Assert.assertNotNull(roleRequest);
 		Assert.assertNotNull(roleRequest.getId());
+		Assert.assertEquals(RoleRequestState.CONCEPT, roleRequest.getState());
 				
 		IdmBulkActionDto bulkAction = this.findBulkAction(IdmRoleRequest.class, RoleRequestDeleteBulkAction.NAME);
 		
@@ -62,5 +64,71 @@ public class RoleRequestDeleteBulkActionIntegrationTest extends AbstractBulkActi
 		
 		IdmRoleRequestDto deletedRoleRequest = roleRequestService.get(roleRequest.getId());
 		Assert.assertNull(deletedRoleRequest);
+	}
+	
+	/**
+	 * When already executed in IdM and not in a system,
+	 * request is set canceled on the target system
+	 */
+	@Test
+	public void deleteRoleRequestExecuted() {
+		IdmRoleDto role = this.createRoles(1).get(0);
+		IdmIdentityDto identity = createIdentities(1).get(0);
+		IdmIdentityContractDto contract = getHelper().getPrimeContract(identity);
+		
+		IdmRoleRequestDto roleRequest = roleRequestService.createRequest(contract, role);
+		Assert.assertEquals(RoleRequestState.CONCEPT, roleRequest.getState());
+		getHelper().executeRequest(roleRequest, false);
+		roleRequest = roleRequestService.get(roleRequest);
+		Assert.assertEquals(RoleRequestState.EXECUTED, roleRequest.getState());
+		
+		Assert.assertNotNull(roleRequest);
+		Assert.assertNotNull(roleRequest.getId());
+				
+		IdmBulkActionDto bulkAction = this.findBulkAction(IdmRoleRequest.class, RoleRequestDeleteBulkAction.NAME);
+		
+		Set<UUID> ids = new HashSet<UUID>();
+		ids.add(roleRequest.getId());
+		bulkAction.setIdentifiers(ids);
+		IdmBulkActionDto processAction = bulkActionManager.processAction(bulkAction);
+		checkResultLrt(processAction, 1l, null, null);
+		
+		roleRequest = roleRequestService.get(roleRequest.getId());
+		Assert.assertEquals(OperationState.CANCELED,roleRequest.getSystemState().getState());
+		
+		// request cannot be deleted because it is already executed
+		processAction = bulkActionManager.processAction(bulkAction);
+		checkResultLrt(processAction, null, 1l, null);
+	}
+	
+	
+	/**
+	 * Running request has to be canceled rather than deleted
+	 */
+	@Test
+	public void deleteRoleRequestInProgress() {
+		IdmRoleDto role = this.createRoles(1).get(0);
+		IdmIdentityDto identity = createIdentities(1).get(0);
+		IdmIdentityContractDto contract = getHelper().getPrimeContract(identity);
+		
+		IdmRoleRequestDto roleRequest = roleRequestService.createRequest(contract, role);
+		Assert.assertNotNull(roleRequest);
+		Assert.assertNotNull(roleRequest.getId());
+		Assert.assertEquals(RoleRequestState.CONCEPT, roleRequest.getState());
+		// manual change state to cover particular way the code goes
+		roleRequest.setState(RoleRequestState.IN_PROGRESS);
+		roleRequest = roleRequestService.save(roleRequest);
+		Assert.assertEquals(RoleRequestState.IN_PROGRESS, roleRequest.getState());
+				
+		IdmBulkActionDto bulkAction = this.findBulkAction(IdmRoleRequest.class, RoleRequestDeleteBulkAction.NAME);
+		
+		Set<UUID> ids = new HashSet<UUID>();
+		ids.add(roleRequest.getId());
+		bulkAction.setIdentifiers(ids);
+		IdmBulkActionDto processAction = bulkActionManager.processAction(bulkAction);
+		checkResultLrt(processAction, 1l, null, null);
+		
+		roleRequest = roleRequestService.get(roleRequest.getId());
+		roleRequest.setState(RoleRequestState.CANCELED);
 	}
 }
