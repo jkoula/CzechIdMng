ee14a50f615b5d374f443ad6aff80cae1cfc2bf2
Radek Tomiška <radek.tomiska@bcvsolutions.eu>
2021-10-06 15:07:09 +0200

    #2975 Audit: Handle exception in audit, when referenced entity not found (for entities with Not Found - ignore annotation)

--- Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/audit/rest/impl/IdmAuditController.java
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/audit/rest/impl/IdmAuditController.java
@@ -356,6 +356,8 @@ public class IdmAuditController extends AbstractReadWriteDtoController<IdmAuditD
 				loadedDtos.put(entityId, revision);
 			} catch (IllegalArgumentException ex) {
 				LOG.debug("Class [{}] not found on classpath (e.g. module was uninstalled)", dto.getType(), ex);
+			} catch (Exception ex) {
+				LOG.debug("Type [{}] cannot be mapped to dto.", dto.getType(), ex);
 			}
 		}
 		dto.getEmbedded().put(IdmAudit_.entityId.getName(), revision); // nullable
@@ -373,6 +375,8 @@ public class IdmAuditController extends AbstractReadWriteDtoController<IdmAuditD
 				}
 			} catch (IllegalArgumentException | ClassNotFoundException ex) {
 				LOG.debug("Class [{}] not found on classpath (e.g. module was uninstalled)", dto.getType(), ex);
+			} catch (Exception ex) {
+				LOG.debug("Type [{}] cannot be mapped to dto.", dto.getType(), ex);
 			}
 		}
 		//
@@ -386,6 +390,8 @@ public class IdmAuditController extends AbstractReadWriteDtoController<IdmAuditD
 				dto.getEmbedded().put(IdmAudit_.subOwnerId.getName(), loadedDtos.get(subOwnerId));
 			} catch (IllegalArgumentException ex) {
 				LOG.debug("Class [{}] not found on classpath (e.g. module was uninstalled)", dto.getSubOwnerType(), ex);
+			} catch (Exception ex) {
+				LOG.debug("Type [{}] cannot be mapped to dto.", dto.getType(), ex);
 			}
 		}
 		// For owner, some entities doesn't support owner and subowner.
@@ -398,6 +404,8 @@ public class IdmAuditController extends AbstractReadWriteDtoController<IdmAuditD
 				dto.getEmbedded().put(IdmAudit_.ownerId.getName(), loadedDtos.get(ownerId));
 			} catch (IllegalArgumentException ex) {
 				LOG.debug("Class [{}] not found on classpath (e.g. module was uninstalled)", dto.getSubOwnerType(), ex);
+			} catch (Exception ex) {
+				LOG.debug("Type [{}] cannot be mapped to dto.", dto.getType(), ex);
 			}
 		}
 		// Fill embedded contract for FE agenda (prevent to load contract for each row).
