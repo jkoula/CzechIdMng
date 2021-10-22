f96ddc62173d02c4f00aef816cae90b88d14037e
Radek Tomiška <radek.tomiska@bcvsolutions.eu>
2021-10-22 13:48:45 +0200

    sonar clean code

--- Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/model/service/impl/DefaultUniversalSearchManager.java
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/model/service/impl/DefaultUniversalSearchManager.java
@@ -1,19 +1,20 @@
 package eu.bcvsolutions.idm.core.model.service.impl;
 
-import com.google.common.collect.Lists;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
 import eu.bcvsolutions.idm.core.api.dto.AbstractDto;
 import eu.bcvsolutions.idm.core.api.dto.UniversalSearchTypeDto;
 import eu.bcvsolutions.idm.core.api.dto.filter.BaseFilter;
 import eu.bcvsolutions.idm.core.api.service.UniversalSearchManager;
 import eu.bcvsolutions.idm.core.eav.api.service.UniversalSearchType;
 import eu.bcvsolutions.idm.core.security.api.service.EnabledEvaluator;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Service;
 
 /**
  * Default universal search manager
@@ -32,20 +33,14 @@ public class DefaultUniversalSearchManager implements UniversalSearchManager {
 	private EnabledEvaluator enabledEvaluator;
 
 	@Override
-	@SuppressWarnings(value = "rawtypes")
 	public List<UniversalSearchType<? extends AbstractDto, ? extends BaseFilter>> getSupportedTypes() {
-		List<UniversalSearchType<?, ?>> results = Lists.newArrayList();
-		List<UniversalSearchType> types = context
+		return context
 				.getBeansOfType(UniversalSearchType.class)
 				.values()
 				.stream()
 				.filter(enabledEvaluator::isEnabled)
 				.sorted(Comparator.comparing(UniversalSearchType::getOrder))
 				.collect(Collectors.toList());
-		for (UniversalSearchType type : types) {
-			results.add(type);
-		}
-		return results;
 	}
 
 	@Override
