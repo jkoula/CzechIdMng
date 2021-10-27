54a071a4b0189bc002a341749068008a9a018bfe
Radek Tomiška <radek.tomiska@bcvsolutions.eu>
2021-10-27 11:57:01 +0200

    #2885 remove less variables usage

--- Realization/frontend/czechidm-core/src/components/basic/ContentHeader/ContentHeader.js
+++ Realization/frontend/czechidm-core/src/components/basic/ContentHeader/ContentHeader.js
@@ -2,55 +2,79 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 //
+import { makeStyles } from '@material-ui/core/styles';
+//
 import AbstractComponent from '../AbstractComponent/AbstractComponent';
 import Icon from '../Icon/Icon';
 import HelpIcon from '../HelpIcon/HelpIcon';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: '0 0 10px 0',
+    padding: '10px 0',
+    borderBottomWidth: 1,
+    borderBottomStyle: 'solid',
+    borderBottomColor: theme.palette.secondary.main,
+    '& h2': {
+      margin: 0,
+      padding: 0,
+      lineHeight: '25px',
+      fontSize: '20px'
+    },
+    '&.marginable': {
+      marginLeft: -15,
+      marginRight: -15,
+      paddingLeft: 15,
+      paddingRight: 15
+    }
+  }
+}));
+
 /**
- * Content header
+ * Content header.
  *
  * @author Radek Tomiška
  */
-class ContentHeader extends AbstractComponent {
-
-  render() {
-    const { rendered, showLoading, children, className, help, text, icon, buttons, ...others } = this.props;
-    if (!rendered) {
-      return null;
-    }
-    if (!text && !children) {
-      return null;
-    }
-
-    const classNames = classnames(
-      'content-header',
-      className
-    );
-    //
-    return (
-      <div className={ classNames } { ...others }>
-        <h2>
-          {
-            showLoading
-            ?
-            <Icon type="fa" icon="refresh" showLoading className="icon-loading"/>
-            :
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <Icon value={ icon } style={{ marginRight: 7 }}/>
-              <div style={{ flex: 1 }}>
-                { text }
-                { children }
-              </div>
-              <div style={{ fontSize: '0.7em' }}>
-                { buttons }
-              </div>
-              <HelpIcon content={ help }/>
-            </div>
-          }
-        </h2>
-      </div>
-    );
+export default function ContentHeader(props) {
+  const { rendered, showLoading, children, className, help, text, icon, buttons, ...others } = props;
+  const classes = useStyles();
+  //
+  if (!rendered) {
+    return null;
   }
+  if (!text && !children) {
+    return null;
+  }
+
+  const classNames = classnames(
+    'content-header',
+    classes.root,
+    className
+  );
+  //
+  return (
+    <div className={ classNames } { ...others }>
+      <h2>
+        {
+          showLoading
+          ?
+          <Icon type="fa" icon="refresh" showLoading className="icon-loading"/>
+          :
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Icon value={ icon } style={{ marginRight: 7 }}/>
+            <div style={{ flex: 1 }}>
+              { text }
+              { children }
+            </div>
+            <div style={{ fontSize: '0.7em' }}>
+              { buttons }
+            </div>
+            <HelpIcon content={ help }/>
+          </div>
+        }
+      </h2>
+    </div>
+  );
 }
 
 ContentHeader.propTypes = {
@@ -69,5 +93,3 @@ ContentHeader.propTypes = {
 ContentHeader.defaultProps = {
   ...AbstractComponent.defaultProps
 };
-
-export default ContentHeader;
--- Realization/frontend/czechidm-core/src/components/basic/PageHeader/PageHeader.js
+++ Realization/frontend/czechidm-core/src/components/basic/PageHeader/PageHeader.js
@@ -2,55 +2,71 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 //
+import { makeStyles } from '@material-ui/core/styles';
+//
 import AbstractComponent from '../AbstractComponent/AbstractComponent';
 import Icon from '../Icon/Icon';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: '0 0 10px 0',
+    padding: '0 0 10px 0',
+    borderBottomWidth: 1,
+    borderBottomStyle: 'solid',
+    borderBottomColor: theme.palette.primary.main,
+    '& h1': {
+      margin: 0,
+      padding: 0,
+      lineHeight: 1
+    }
+  }
+}));
+
 /**
- * Page header
+ * Page header.
  *
  * @author Radek Tomiška
  */
-class PageHeader extends AbstractComponent {
-
-  render() {
-    const {
-      rendered,
-      showLoading,
-      icon,
-      children,
-      className,
-      text,
-      ...others
-    } = this.props;
-    //
-    if (!rendered) {
-      return null;
-    }
-    //
-    const classNames = classnames(
-      'page-header',
-      className
-    );
-    return (
-      <div className={ classNames } { ...others }>
-        <h1>
-          {
-            showLoading
-            ?
-            <Icon type="fa" icon="refresh" showLoading className="icon-loading"/>
-            :
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <Icon value={ icon } style={{ marginRight: 7 }}/>
-              <div style={{ flex: 1 }}>
-                { text }
-                { children }
-              </div>
-            </div>
-          }
-        </h1>
-      </div>
-    );
+export default function PageHeader(props) {
+  const {
+    rendered,
+    showLoading,
+    icon,
+    children,
+    className,
+    text,
+    ...others
+  } = props;
+  const classes = useStyles();
+  //
+  if (!rendered) {
+    return null;
   }
+  //
+  const classNames = classnames(
+    'page-header',
+    classes.root,
+    className
+  );
+  return (
+    <div className={ classNames } { ...others }>
+      <h1>
+        {
+          showLoading
+          ?
+          <Icon type="fa" icon="refresh" showLoading className="icon-loading"/>
+          :
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Icon value={ icon } style={{ marginRight: 7 }}/>
+            <div style={{ flex: 1 }}>
+              { text }
+              { children }
+            </div>
+          </div>
+        }
+      </h1>
+    </div>
+  );
 }
 
 PageHeader.propTypes = {
@@ -68,5 +84,3 @@ PageHeader.propTypes = {
 PageHeader.defaultProps = {
   ...AbstractComponent.defaultProps
 };
-
-export default PageHeader;
