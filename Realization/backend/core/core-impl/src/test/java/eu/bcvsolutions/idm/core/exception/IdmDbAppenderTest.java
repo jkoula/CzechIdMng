34745fc2cd0c0fd2b4b7fb089d98cd9a053ba319
Peter Štrunc <peter.strunc@bcvsolutions.eu>
2022-02-10 12:14:50 +0100

    [#3036] Make test public

--- Realization/backend/core/core-impl/src/test/java/eu/bcvsolutions/idm/core/exception/IdmDbAppenderTest.java
+++ Realization/backend/core/core-impl/src/test/java/eu/bcvsolutions/idm/core/exception/IdmDbAppenderTest.java
@@ -23,7 +23,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Component
-class IdmDbAppenderTest extends AbstractIntegrationTest {
+public class IdmDbAppenderTest extends AbstractIntegrationTest {
 
 
 
