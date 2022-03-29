b53998d1138bad6e6e422a31385845072fec67b4
tomdoischer <tomas.doischer@bcvsolutions.eu>
2022-03-29 10:12:30 +0200

    3055, minor changes in report by report type evaluator

--- Realization/backend/rpt/rpt-impl/src/main/java/eu/bcvsolutions/idm/rpt/security/evaluator/ReportByReportTypeEvaluator.java
+++ Realization/backend/rpt/rpt-impl/src/main/java/eu/bcvsolutions/idm/rpt/security/evaluator/ReportByReportTypeEvaluator.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Component;
 
 import com.google.common.collect.Lists;
 
-import eu.bcvsolutions.idm.core.eav.api.domain.BaseFaceType;
 import eu.bcvsolutions.idm.core.eav.api.domain.PersistentType;
 import eu.bcvsolutions.idm.core.eav.api.dto.IdmFormAttributeDto;
 import eu.bcvsolutions.idm.core.security.api.domain.AuthorizationPolicy;
@@ -37,7 +36,7 @@ import eu.bcvsolutions.idm.rpt.service.DefaultReportManager;
  *
  */
 @Component(ReportByReportTypeEvaluator.EVALUATOR_NAME)
-@Description("")
+@Description("Permission to reports by report executor type.")
 public class ReportByReportTypeEvaluator extends AbstractAuthorizationEvaluator<RptReport> {
 
 	private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(ReportByReportTypeEvaluator.class);
@@ -83,8 +82,7 @@ public class ReportByReportTypeEvaluator extends AbstractAuthorizationEvaluator<
 	public List<IdmFormAttributeDto> getFormAttributes() {
 		IdmFormAttributeDto reportTypeFormAttribute = new IdmFormAttributeDto(PARAMETER_REPORT_TYPE, 
 				PARAMETER_REPORT_TYPE, 
-				PersistentType.TEXT, 
-				BaseFaceType.TEXTAREA);
+				PersistentType.SHORTTEXT);
 		reportTypeFormAttribute.setRequired(true);
 		return Lists.newArrayList(reportTypeFormAttribute);
 	}
