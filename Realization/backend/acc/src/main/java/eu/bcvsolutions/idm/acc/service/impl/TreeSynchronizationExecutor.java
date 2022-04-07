e8d173cd20c0d54d7c06385c9877d1b917604f68
Roman Kučera <roman.kucera@bcvsolutions.eu>
2022-04-07 09:51:42 +0200

    #2613 removed unused import

--- Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/service/impl/TreeSynchronizationExecutor.java
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/service/impl/TreeSynchronizationExecutor.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
