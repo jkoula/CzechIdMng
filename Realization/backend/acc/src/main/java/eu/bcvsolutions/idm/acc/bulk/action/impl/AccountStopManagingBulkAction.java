a1c1d0da5f11d4ded133aa62b98e0ff235672f7f
tomdoischer <tomas.doischer@bcvsolutions.eu>
2022-08-02 08:12:16 +0200

    3168, stop managing accounts bulk action initial implementation

---
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/bulk/action/impl/AccountStopManagingBulkAction.java
@@ -0,0 +1,56 @@
+package eu.bcvsolutions.idm.acc.bulk.action.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
+import org.springframework.stereotype.Component;
+
+import com.google.common.collect.Lists;
+
+import eu.bcvsolutions.idm.acc.AccModuleDescriptor;
+import eu.bcvsolutions.idm.acc.domain.AccGroupPermission;
+import eu.bcvsolutions.idm.acc.dto.AccAccountDto;
+import eu.bcvsolutions.idm.acc.dto.filter.AccAccountFilter;
+import eu.bcvsolutions.idm.acc.event.AccountEvent;
+import eu.bcvsolutions.idm.acc.event.AccountEvent.AccountEventType;
+import eu.bcvsolutions.idm.acc.service.api.AccAccountService;
+import eu.bcvsolutions.idm.core.api.bulk.action.AbstractBulkAction;
+import eu.bcvsolutions.idm.core.api.domain.OperationState;
+import eu.bcvsolutions.idm.core.api.entity.OperationResult;
+import eu.bcvsolutions.idm.core.api.service.EntityEventManager;
+import eu.bcvsolutions.idm.core.api.service.ReadWriteDtoService;
+import eu.bcvsolutions.idm.core.security.api.domain.Enabled;
+
+@Enabled(AccModuleDescriptor.MODULE_ID)
+@Component(AccountStopManagingBulkAction.NAME)
+@Description("Stops managing an account (delete without provisioning)")
+public class AccountStopManagingBulkAction extends AbstractBulkAction<AccAccountDto, AccAccountFilter> {
+
+	public static final String NAME = "account-stop-managing-bulk-action";
+
+	@Autowired
+	private AccAccountService accountService;
+	@Autowired
+	private EntityEventManager entityEventManager;
+
+	@Override
+	public ReadWriteDtoService<AccAccountDto, AccAccountFilter> getService() {
+		return accountService;
+	}
+
+	@Override
+	protected OperationResult processDto(AccAccountDto dto) {
+		AccountEvent event = new AccountEvent(AccountEventType.DELETE, dto, 
+				Map.of(AccAccountService.DELETE_TARGET_ACCOUNT_PROPERTY, false));
+		entityEventManager.process(event);
+		
+		return new OperationResult(OperationState.EXECUTED);
+	}
+	
+	@Override
+	protected List<String> getAuthoritiesForEntity() {
+		return Lists.newArrayList(AccGroupPermission.ACCOUNT_DELETE);
+	}
+}
