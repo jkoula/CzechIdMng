ea1b490240ae970573dbe817cda0a14fdf408e5d
Roman Kučera <roman.kucera@bcvsolutions.eu>
2022-08-17 12:03:42 +0200

    #3159 - added flyway script

---
+++ Realization/backend/acc/src/main/resources/eu/bcvsolutions/idm/acc/sql/postgresql/V12_03_003__account-type.sql
@@ -0,0 +1,28 @@
+--
+-- CzechIdM 12.3.0 Flyway script
+-- BCV solutions s.r.o.
+--
+-- Account entity - remove account type column
+alter table acc_account
+    DROP COLUMN account_type;
+
+-- Account entity audit - remove account type column
+alter table acc_account_a
+    DROP COLUMN account_type;
+
+alter table acc_account_a
+    DROP COLUMN account_type_m;
+
+-- System mapping entity - add account type column
+alter table sys_system_mapping
+    add column
+        account_type varchar(255);
+
+-- System mapping entity audit - add account type column
+alter table sys_system_mapping_a
+    add column
+        account_type varchar(255);
+
+alter table sys_system_mapping_a
+    add column
+        account_type_m boolean;
