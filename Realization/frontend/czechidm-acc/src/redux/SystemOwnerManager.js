245d7c3645b9b42519146a1bc47f643751431339
Evgeniya <evgeniya.gorina@bcvsolutions.eu>
2022-09-30 09:21:47 +0200

    fome extra files which were lost before

---
+++ Realization/frontend/czechidm-acc/src/redux/SystemOwnerManager.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+import { SystemOwnerService } from '../services';
+import EntityManager from 'czechidm-core/src/redux/data/EntityManager';
+
+
+export default class SystemOwnerManager extends EntityManager{
+
+    constructor() {
+        super();
+        this.service = new SystemOwnerService();
+      }
+    
+      getService() {
+        return this.service;
+      }
+    
+      getEntityType() {
+        return 'Owners';
+      }
+
+      getCollectionType() {
+        return 'systemOwners';
+      }
+   
+}
