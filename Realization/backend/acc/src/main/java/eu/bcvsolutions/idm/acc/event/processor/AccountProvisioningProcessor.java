b00a9302769fd327d8eadb3fd3bf7a966b82ffbe
Roman Kučera <roman.kucera@bcvsolutions.eu>
2022-10-11 14:47:35 +0200

    #3164 - processor for provisioning after EAV for accounts are saved

---
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/event/processor/AccountProvisioningProcessor.java
@@ -0,0 +1,72 @@
+package eu.bcvsolutions.idm.acc.event.processor;
+
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
+import org.springframework.stereotype.Component;
+
+import eu.bcvsolutions.idm.acc.dto.AccAccountDto;
+import eu.bcvsolutions.idm.acc.entity.AccAccount;
+import eu.bcvsolutions.idm.acc.service.api.AccAccountService;
+import eu.bcvsolutions.idm.acc.service.api.ProvisioningService;
+import eu.bcvsolutions.idm.core.api.event.CoreEvent;
+import eu.bcvsolutions.idm.core.api.event.CoreEvent.CoreEventType;
+import eu.bcvsolutions.idm.core.api.event.CoreEventProcessor;
+import eu.bcvsolutions.idm.core.api.event.DefaultEventResult;
+import eu.bcvsolutions.idm.core.api.event.EntityEvent;
+import eu.bcvsolutions.idm.core.api.event.EventResult;
+import eu.bcvsolutions.idm.core.eav.api.dto.IdmFormInstanceDto;
+
+/**
+ * EAV account save. After it's saved we will do provisioning
+ * 
+ * @author Roman Kucera
+ */
+@Component("accAccountProvisioningProcessor")
+@Description("Do provisioning after eav is saved")
+public class AccountProvisioningProcessor extends CoreEventProcessor<IdmFormInstanceDto> {
+
+	private static final String PROCESSOR_NAME = "account-provisioning-processor";
+
+	@Autowired
+	private AccAccountService accountService;
+	@Autowired
+	private ProvisioningService provisioningService;
+
+	@Autowired
+	public AccountProvisioningProcessor() {
+		super(CoreEventType.UPDATE);
+	}
+
+	@Override
+	public boolean conditional(EntityEvent<IdmFormInstanceDto> event) {
+		return super.conditional(event) && event.getContent().getOwnerType().getCanonicalName().equals(AccAccount.class.getCanonicalName());
+	}
+
+	@Override
+	public EventResult<IdmFormInstanceDto> process(EntityEvent<IdmFormInstanceDto> event) {
+		Serializable ownerId = event.getContent().getOwnerId();
+
+		AccAccountDto accountDto = accountService.get(ownerId);
+		provisioningService.doProvisioning(accountDto);
+
+		return new DefaultEventResult<>(event, this);
+	}
+
+	@Override
+	public String getName() {
+		return PROCESSOR_NAME;
+	}
+
+	@Override
+	public int getOrder() {
+		return CoreEvent.DEFAULT_ORDER + 500;
+	}
+
+	@Override
+	public boolean isDisableable() {
+		return false;
+	}
+
+}
