7dd63a8ccb68db22f382060705a1c79bdd8be3f1
Peter Štrunc <peter.strunc@bcvsolutions.eu>
2023-03-21 15:50:12 +0100

    [#3295] Add import for Lazy annotation

--- Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/service/impl/DefaultSysSystemService.java
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/service/impl/DefaultSysSystemService.java
@@ -21,6 +21,7 @@ import org.apache.http.util.Asserts;
 import org.identityconnectors.framework.common.objects.OperationOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
