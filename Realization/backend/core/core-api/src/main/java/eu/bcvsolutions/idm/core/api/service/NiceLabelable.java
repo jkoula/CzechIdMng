629e708fef116bf5f9b7e064bfa2c40945b42c0f
Peter Štrunc <peter.strunc@bcvsolutions.eu>
2023-04-18 09:45:01 +0200

    ú[#3334] Add interface for service supporting nice label creation

---
+++ Realization/backend/core/core-api/src/main/java/eu/bcvsolutions/idm/core/api/service/NiceLabelable.java
@@ -0,0 +1,18 @@
+package eu.bcvsolutions.idm.core.api.service;
+
+import eu.bcvsolutions.idm.core.api.dto.BaseDto;
+import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
+
+/**
+ * @author Peter Štrunc <github.com/peter-strunc>
+ */
+public interface NiceLabelable<T extends BaseDto> {
+    /**
+     * Better "toString".
+     * Returns identity's fullName with titles if lastName is not blank, otherwise returns username
+     *
+     * @param identity
+     * @return
+     */
+    String getNiceLabel(T identity);
+}
